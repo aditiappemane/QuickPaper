f5d61c7b9e97b8e86246eef13b220dd138e84f05
kishlay <kishlay141@gmail.com>
2025-06-27 00:13:17 +0530

    updated the spinner.tsx for eliminating the error

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,17 +1,17 @@
-import Motion from "@/lib/motion";
+import { motion } from "@/lib/motion";
 
 export default function Spinner() {
   return (
-    <Motion.div
+    <motion.div
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       className="flex justify-center py-12"
     >
-      <Motion.div
+      <motion.div
         animate={{ rotate: 360 }}
         transition={{ duration: 0.8, repeat: Infinity, ease: "linear" }}
         className="w-12 h-12 border-t-4 border-blue-600 border-r-4 border-b-4 border-transparent rounded-full"
       />
-    </Motion.div>
+    </motion.div>
   );
 }
