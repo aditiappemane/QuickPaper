d8c8e783052c17f0163d57f7ff10a790925ad8fa
kishlay <kishlay141@gmail.com>
2025-06-27 00:03:37 +0530

    updated the template

--- src/app/Template.tsx
+++ src/app/Template.tsx
@@ -1,9 +1,9 @@
 "use client";
-import { motion } from "framer-motion";
+import Motion from "@/lib/motion";
 
 export default function Template({ children }: { children: React.ReactNode }) {
   return (
-    <motion.div
+    <Motion.div
       initial={{ opacity: 0, y: 16 }}
       animate={{ opacity: 1, y: 0 }}
       exit={{ opacity: 0, y: 16 }}
@@ -11,6 +11,6 @@ export default function Template({ children }: { children: React.ReactNode }) {
       className="w-full"
     >
       {children}
-    </motion.div>
+    </Motion.div>
   );
 }
